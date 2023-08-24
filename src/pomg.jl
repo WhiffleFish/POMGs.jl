3c9d992cffc786d85d89fa351df728b9020f4b2a
WhiffleFish <tylertronics@gmail.com>
2023-08-24 13:59:45 -0600

    fix docstrings + add `player_actions`

--- src/pomg.jl
+++ src/pomg.jl
@@ -58,11 +58,38 @@ function players end
 
 players(::POMG) = 1:2
 
+"""
+    actions(game)
+
+Returns the action space for each player (A1, A2)
+
+--
+    actions(game, s)
+
+Returns the actions that can be taken for each player (A1, A2) at state `s`
+
+"""
 function actions end
 
 actions(g::POMG, s) = actions(g)
 
 actions(g::POMG, s, p) = actions(g, s)[p]
+
+"""
+    player_actions(game, p)
+
+Returns the action space for player `p`
+
+--
+    player_actions(game, s, p)
+
+Returns the actions that can be taken by player `p` at state `s`
+"""
+function player_actions end
+
+player_actions(game, p) = actions(game)[p]
+
+player_actions(game, s, p) = actions(game, s)[p]
 
 function gen end
 
