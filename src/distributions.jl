9d1b5abb9afec69f7faef88a6d115d3ed76e92cd
WhiffleFish <tylertronics@gmail.com>
2023-10-20 10:48:43 -0600

    dispatch on support for `ProductDistribution`

--- src/distributions.jl
+++ src/distributions.jl
@@ -13,4 +13,6 @@ POMDPs.pdf(d::ProductDistribution, x) = mapreduce(*, x, d.dists) do x_i, dist
     POMDPs.pdf(dist, x_i)
 end
 
+POMDPs.support(d::ProductDistribution) = Iterators.product((support(_d) for _d ∈ d)...)
+
 Random.rand(rng::AbstractRNG, d::Random.SamplerTrivial{<:ProductDistribution}) = map(_d->rand(rng, _d), d[].dists)
