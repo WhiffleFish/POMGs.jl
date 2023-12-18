58ef991b62d8d38868880fbf34f476b6762b9ce8
WhiffleFish <tylertronics@gmail.com>
2023-12-18 11:32:51 -0700

    product distribution n=2 loop unrolling

--- src/distributions.jl
+++ src/distributions.jl
@@ -13,6 +13,9 @@ POMDPs.pdf(d::ProductDistribution, x) = mapreduce(*, x, d.dists) do x_i, dist
     POMDPs.pdf(dist, x_i)
 end
 
+# loop unrolling
+POMDPs.pdf(d::ProductDistribution{<:NTuple{2,Any}}, x) = POMDPs.pdf(d[1], x[1]) * POMDPs.pdf(d[2], x[2])
+
 POMDPs.support(d::ProductDistribution) = Iterators.product((support(_d) for _d ∈ d)...)
 
 Random.rand(rng::AbstractRNG, d::Random.SamplerTrivial{<:ProductDistribution}) = map(_d->rand(rng, _d), d[].dists)
